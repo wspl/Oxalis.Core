0b30f2d502c393f21b0c0a5324476f8d870d7fa9
Plutonist <widesense.plqws@gmail.com>
2016-06-07 13:51:05 +0800

    No comment

--- IrisCore/Program.cs
+++ IrisCore/Program.cs
@@ -22,21 +22,78 @@ namespace Iris.Core
         static void Main(string[] args)
         {
             var cjk = new CJKTranslator();
-            var testString = "Visual Studio 2015";
+            var testString = "网易云音乐";
 
             var rs = cjk.TranslateToList(testString.ToLower());
 
             var pool = new List<int[]>();
+
+            var beforDT = DateTime.Now;
+
+            // Get Trait
             foreach (var ctx in rs)
             {
                 SearchWorker(pool, ctx, new int[Keyword.Length].Select((v) => -1).ToArray(), 0, 0);
             }
 
+            Console.WriteLine("Get Trait Array Count：{0}", pool.Count);
 
+            // Calculate First Chars Pos Score
+            var fcPosList = new List<int> { 0 };
+            var currentFcPos = 0;
+            while (true)
+            {
+                var nextSpacePos = rs[0].IndexOf(' ', currentFcPos);
+                currentFcPos = nextSpacePos + 1;
+                if (nextSpacePos == -1) break;
+                fcPosList.Add(currentFcPos);
+            }
+
+            var fcScores = new List<double>();
+            foreach (var trait in pool)
+            {
+                var matchCount = 0;
+                foreach (var pos in trait)
+                {
+                    if (fcPosList.Contains(pos))
+                    {
+                        matchCount += 1;
+                    }
+                }
+                fcScores.Add(matchCount / (double)trait.Length);
+            }
+            var fcScore = fcScores.Count > 0 ? fcScores.Max() : 0;
+
+            // Calculate Continuity Score
+            var conScores = new List<double>();
+            foreach (var trait in pool)
+            {
+                var continueCount = 0;
+                for (var i = 1; i < trait.Length; i += 1)
+                {
+                    if (trait[i] == trait[i - 1] + 1)
+                    {
+                        continueCount += 1;
+                    }
+                }
+                conScores.Add(continueCount / (double)trait.Length);
+            }
+            var conScore = conScores.Count > 0 ? conScores.Max() : 0;
+
+            var aveScore = (fcScore + conScore) / 2;
+
+            var afterDT = DateTime.Now;
+            var ts = afterDT.Subtract(beforDT);
+            Console.WriteLine("FirstChar Score: {0}", fcScore);
+            Console.WriteLine("Continuity Score: {0}", conScore);
+            Console.WriteLine("Final Score: {0}", aveScore);
+            Console.WriteLine("Using {0} ms.", ts.TotalMilliseconds);
+            Console.ReadKey();
         }
 
-        static string Keyword = "vs2015".ToLower();
+        static string Keyword = "wyiyuyyue".ToLower();
 
+        // TODO: Demand Optimization
         static void SearchWorker(List<int[]> pool, string src, int[] lastTrait, int kwPos, int lastSearchPos)
         {
             if (lastTrait[lastTrait.Length - 1] == -1)
